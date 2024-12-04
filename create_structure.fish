d0b10db191f9aece7dfbe07cf33fb5033f25b12f
tom wawer <sq5rix@gmail.com>
2024-12-04 16:58:22 +0100

    structure

---
+++ create_structure.fish
@@ -0,0 +1,9 @@
+mkdir swarm
+cd swarm
+mkdir -p go/cmd/agent1
+mkdir -p go/cmd/agent2
+mkdir -p go/internal/common
+mkdir -p go/internal/agents
+mkdir docker
+mkdir rabbitmq
+mkdir -p services/logs
---
+++ swarm/go/cmd/agent1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "github.com/username/myproject/internal/config"
+    "github.com/username/myproject/pkg/logger"
+)
+
+func main() {
+    // Program 1 implementation
+}
+
